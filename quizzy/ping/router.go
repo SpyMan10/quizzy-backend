1d2eab48c9dcfd1f8092a0ab73f94d1472ed3631
SpyMan10 <dev.spyman@gmail.com>
2025-02-11 14:23:23 +0100

    merge: Ikari-421/quizzy-backend@main -> main

--- quizzy/ping/router.go
+++ quizzy/ping/router.go
@@ -9,28 +9,20 @@ func ConfigureRoutes(rt *gin.RouterGroup) {
 }
 
 func ping(c *gin.Context) {
-<<<<<<< HEAD
-	// Teste de la reponse FireBAse
-	payload := make(map[string]interface{})
-
-	dataStatus := false // A remplacer par un boolean de test de connexion a la base de donnée
-	statusHtml := 200
-	printHtml := "OK"
-	printDatabase := "OK"
-
-	if !dataStatus {
-		statusHtml = 500
-		printHtml = "Partial"
-		printDatabase = "KO"
+	if _, exists := c.Get("firebase-services"); !exists {
+		c.JSON(500, gin.H{
+			"status": "Partial",
+			"details": gin.H{
+				"database": "KO",
+			},
+		})
+		return
 	}
 
-	payload["status"] = printHtml
-	payload["details"] = map[string]string{"database": printDatabase}
-
-	c.JSON(statusHtml, payload)
-=======
-	c.JSON(500, gin.H{
+	c.JSON(200, gin.H{
 		"status": "OK",
+		"details": gin.H{
+			"database": "OK",
+		},
 	})
->>>>>>> 83e3dbb9155307f65aef0c2bea5438d96e45d3c5
 }
