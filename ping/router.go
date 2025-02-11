a71b7f8c7e2e9e738ed4c8e0d1d9761349943637
Ikari-421 <maximilien.auclair@gmail.com>
2025-02-11 10:06:41 +0100

    Fix Ping

--- ping/router.go
+++ ping/router.go
@@ -1,17 +1,13 @@
 package ping
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Setup(rt *gin.Engine) {
 	rt.GET("/ping", func(c *gin.Context) {
-		if http.StatusOK == 200 {
-			c.JSON(200, gin.H{
-				"status": "OK",
-			})
-		}
+		c.JSON(200, gin.H{
+			"status": "OK",
+		})
 	})
 }
