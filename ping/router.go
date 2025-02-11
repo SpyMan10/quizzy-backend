b66f44f7906e322ae166442577aa821f1347fde6
Ikari-421 <maximilien.auclair@gmail.com>
2025-02-11 13:39:32 +0100

    Ajout de la logique test connexion Database

--- ping/router.go
+++ ping/router.go
@@ -9,7 +9,22 @@ func Setup(rt *gin.Engine) {
 }
 
 func ping(c *gin.Context) {
-	c.JSON(500, gin.H{
-		"status": "OK",
-	})
+	// Teste de la reponse FireBAse
+	payload := make(map[string]interface{})
+
+	dataStatus := false // A remplacer par un boolean de test de connexion a la base de donnée
+	statusHtml := 200
+	printHtml := "OK"
+	printDatabase := "OK"
+
+	if !dataStatus {
+		statusHtml = 500
+		printHtml = "Partial"
+		printDatabase = "KO"
+	}
+
+	payload["status"] = printHtml
+	payload["details"] = map[string]string{"database": printDatabase}
+
+	c.JSON(statusHtml, payload)
 }
